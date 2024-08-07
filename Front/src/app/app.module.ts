253451608fb338c50dde6c37fefb2667d1bd39c7
Juan Fernando Obando <jobandomh93@gmail.com>
2024-08-06 19:01:25 -0500

    Crear api de Roles y vista, para crear los mismos

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -8,9 +8,13 @@ import { FormsModule } from '@angular/forms'; // Importa FormsModule aquí
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './theme/shared/shared.module';
+import { RolesComponent } from './settings/roles/roles.component'; // Asegúrate de importar el componente
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    RolesComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- api_laravel/routes/api.php
+++ api_laravel/routes/api.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\RoleController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -20,15 +20,11 @@ use Illuminate\Support\Facades\Route;
 // });
 
  
-Route::group([
- 
-    // 'middleware' => 'api',
-    'prefix' => 'auth'
- 
-], function ($router) {
+Route::group(['prefix' => 'auth'], function ($router) {
     Route::post('/register', [AuthController::class, 'register'])->name('register');
     Route::post('/login', [AuthController::class, 'login'])->name('login');
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::post('/refresh', [AuthController::class, 'refresh'])->name('refresh');
     Route::post('/me', [AuthController::class, 'me'])->name('me');
 });
+Route::post('/roles', [RoleController::class, 'store']);
